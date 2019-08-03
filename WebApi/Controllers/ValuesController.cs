2b026dcdb3ad1c94502970ae363d1d5e94db1c7d
Siddharth Jadhav <siddharth.jadhav2093@gmail.com>
2019-08-03 15:27:38 +0530

    Add get post put and delete request

--- WebApi/Controllers/ValuesController.cs
+++ WebApi/Controllers/ValuesController.cs
@@ -10,36 +10,49 @@ namespace WebApi.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private List<string> valuesObj = new List<string>()
+        {
+            "value1",
+            "value2",
+            "value3"
+        };
+
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return valuesObj;
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            return valuesObj[id];
         }
 
         // POST api/values
         [HttpPost]
         public void Post([FromBody] string value)
         {
+            valuesObj.Add(value);
+            Console.WriteLine("ValueObj : ", valuesObj);
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
+            valuesObj[id] = value;
+            Console.WriteLine("ValueObj : ", valuesObj);
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            valuesObj.RemoveAt(id);
+            Console.WriteLine("ValueObj : ", valuesObj);
         }
     }
 }
